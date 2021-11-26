34397abf16d8ec49af83f91abc030b18b7a30ae6
Nikita Konev <nikit.cpp@yandex.ru>
2021-11-27 00:58:48 +0300

    initial liqui

--- src/main/java/name/nkonev/spring/demo/SpringNativeDemoApplication.java
+++ src/main/java/name/nkonev/spring/demo/SpringNativeDemoApplication.java
@@ -1,18 +1,17 @@
 package name.nkonev.spring.demo;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ResourceHint;
 
-@NativeHint(trigger = Flyway.class, resources = {@ResourceHint(patterns={
-        "migration/.*",
-////        "/migration/.*",
-//        "resources/migration/.*",
-//        ".*sql$",
-        "org/flywaydb/core/internal/version.txt"
-})})
+//@NativeHint(trigger = Flyway.class, resources = {@ResourceHint(patterns={
+//        "migration/.*",
+//////        "/migration/.*",
+////        "resources/migration/.*",
+////        ".*sql$",
+//        "org/flywaydb/core/internal/version.txt"
+//})})
 @SpringBootApplication
 public class SpringNativeDemoApplication {
 
