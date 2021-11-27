938484a2f17e7ebf01f2d65c26fb44f2713c41e1
Nikita Konev <nikit.cpp@yandex.ru>
2021-11-27 04:03:58 +0300

    initially working Liquibase yaml + sql

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 ### Reference Documentation
 For further reference, please consider the following sections:
 
-* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
-* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.6.0/maven-plugin/reference/html/)
-* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.6.0/maven-plugin/reference/html/#build-image)
 * [Spring Native Reference Guide](https://docs.spring.io/spring-native/docs/current/reference/htmlsingle/)
 
-### Additional Links
-These additional references should also help you:
-
-* [Configure the Spring AOT Plugin](https://docs.spring.io/spring-native/docs/0.11.0-RC1/reference/htmlsingle/#spring-aot-maven)
 
 ## Spring Native
 
@@ -49,7 +42,7 @@ $ target/spring-native-demo
 ```
 
 
-# My
+# My experiments (works liquibase yaml + sql)
 
 ```
 su -
--- src/main/java/name/nkonev/spring/demo/SpringNativeDemoApplication.java
+++ src/main/java/name/nkonev/spring/demo/SpringNativeDemoApplication.java
@@ -1,22 +1,42 @@
 package name.nkonev.spring.demo;
 
+import liquibase.change.core.SQLFileChange;
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.*;
 
-//@NativeHint(trigger = Flyway.class, resources = {@ResourceHint(patterns={
-//        "migration/.*",
-//////        "/migration/.*",
-////        "resources/migration/.*",
-////        ".*sql$",
-//        "org/flywaydb/core/internal/version.txt"
-//})})
+@NativeHint(
+        trigger = SpringLiquibase.class,
+        types = {
+                // Native reflection configuration for <init>() is missing.
+                // https://github.com/spring-projects-experimental/spring-native/commit/10001243782238ba93ef62f982350c3f3252c31d
+                @TypeHint(types = {
+                        liquibase.configuration.LiquibaseConfiguration.class,
+                        liquibase.logging.core.LogServiceFactory.class,
+                        liquibase.license.LicenseServiceFactory.class,
+                        liquibase.executor.ExecutorService.class,
+                        liquibase.change.ChangeFactory.class,
+                        liquibase.sql.visitor.PrependSqlVisitor.class,
+                        liquibase.sql.visitor.AppendSqlVisitor.class,
+                        liquibase.sql.visitor.RegExpReplaceSqlVisitor.class,
+                        liquibase.sql.visitor.ReplaceSqlVisitor.class,
+                        liquibase.hub.HubServiceFactory.class,
+                },
+                        access = AccessBits.LOAD_AND_CONSTRUCT),
+                // Getters and setters
+                @TypeHint(types = {
+                        SQLFileChange.class
+                },
+                        access = AccessBits.LOAD_AND_CONSTRUCT_AND_PUBLIC_METHODS
+                ),
+        }
+)
 @SpringBootApplication
 public class SpringNativeDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringNativeDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeDemoApplication.class, args);
+    }
 
 }
